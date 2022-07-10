b3a9c546ded99fa95bbc9b1ff4043eebbb00fe7b
VARUN46 <vsetia46@gmail.com>
2022-07-10 22:29:30 +0530

    App name configuration added

---
+++ HomeExpenseApp/App/App/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Text;
+
+namespace App
+{
+    public class AppSettings
+    {
+        public const string ApplicationName = "TrackXpenses";
+    }
+}
