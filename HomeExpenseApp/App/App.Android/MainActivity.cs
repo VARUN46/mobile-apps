abd1e0aa6b279e557c67d27e45a0adc2ce069975
VARUN46 <vsetia46@gmail.com>
2022-07-11 21:02:48 +0530

    Package updates

--- HomeExpenseApp/App/App.Android/MainActivity.cs
+++ HomeExpenseApp/App/App.Android/MainActivity.cs
@@ -7,7 +7,8 @@ using Android.OS;
 
 namespace App.Droid
 {
-    [Activity(Label = AppSettings.ApplicationName, Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = AppSettings.ApplicationName, 
+        Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
