fa829466ea86ecdca43fd3c046ff26a39bb72ad8
VARUN46 <vsetia46@gmail.com>
2022-08-06 14:20:10 +0530

    ML initial setup

---
+++ HomeExpenseApp/App.Interfaces/AIEngine/IExpensePredictor.cs
@@ -0,0 +1,13 @@
+﻿using App.Entities.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Interfaces.AIEngine
+{
+    public interface IExpensePredictor
+    {
+        bool TrainModel(IEnumerable<ExpenseEntry> expenseEntries);
+        string PredictValue(int weekNumber);
+    }
+}
